1d8b0fbe98fd6a8b00fcb99608a331adf130f2ac
Valkiria0000 <90210008+Valkiria0000@users.noreply.github.com>
2022-07-17 15:10:13 +0300

    Add files via upload

--- js/sliderAdaptiv.js
+++ js/sliderAdaptiv.js
@@ -2,7 +2,7 @@ function sliderAdaptiv() {
   const slider = document.querySelector(".slide");
   const leftBlock = document.querySelector(".section2_content_block1");
 
-  if (window.innerWidth < 990) {
+  if (document.documentElement.clientWidth < 990) {
     leftBlock.children[1].insertAdjacentElement("beforeend", slider);
   } else {
     console.dir(leftBlock);
